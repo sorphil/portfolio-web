3873dffdc06a1563acf3aa0db999544d1179129c
sorphil <psoriano01@hotmail.com>
2024-11-23 22:38:47 -0500

    edited about and added filter exception to emojis

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const html = document.querySelector('html')
     let themeBtn = document.getElementById("navbar-item-theme");
     let images = document.querySelectorAll('img');
+    let emojis = document.querySelectorAll('.emoji')
     let darkTheme = String(localStorage.getItem("darkTheme"));
    
     if(darkTheme == null) darkTheme = false;
@@ -9,6 +10,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
     {
         themeBtn.classList.add('dark-mode-toggled')
         images.forEach((image)=>{
+            image.classList.add('dark-mode-toggled')
+        })
+        emojis.forEach((image)=>{
             image.classList.add('dark-mode-toggled')
         })
 
@@ -19,6 +23,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
     {
         themeBtn.classList.remove('dark-mode-toggled')
             images.forEach((image)=>{
+                image.classList.remove('dark-mode-toggled')
+            })
+            emojis.forEach((image)=>{
                 image.classList.remove('dark-mode-toggled')
             })
             localStorage.setItem("darkTheme", true)
